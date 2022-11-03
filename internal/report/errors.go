055455f972b6f68312f20ac95b1c83b857d329a1
Florian Rey <nervo@nervo.net>
2022-11-03 20:30:26 +0100

    misc: introduce sub report errors

--- internal/report/errors.go
+++ internal/report/errors.go
@@ -2,12 +2,14 @@ package report
 
 func NewError(err error) *Error {
 	return &Error{
-		error: err,
+		error:  err,
+		errors: []error{},
 	}
 }
 
 type Error struct {
 	error
+	errors  []error
 	options []Option
 }
 
@@ -17,6 +19,19 @@ func (err *Error) Unwrap() error {
 
 func (err *Error) Report(report *Report) {
 	report.Compose(err.options...)
+
+	// Sub errors
+	if len(err.errors) > 0 {
+		for _, _err := range err.errors {
+			report.Add(NewErrorReport(_err))
+		}
+	}
+}
+
+func (err *Error) Add(_err error) *Error {
+	err.errors = append(err.errors, _err)
+
+	return err
 }
 
 func (err *Error) WithMessage(message string) *Error {
