9999af8877fdb798c75490c25e70bc33df1d32ec
arthur <arthur.farrugia@elao.com>
2023-03-29 15:49:58 +0200

    [Web] add mui

--- web/app/src/pages/Layout.tsx
+++ web/app/src/pages/Layout.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { AppBar, CssBaseline, Toolbar, Typography } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
 
 export default function Layout() {
-  return <div>
-    <h1>Layout - Web UI</h1>
-    <Outlet />
-  </div>;
+  return <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <AppBar position="fixed" style={{ background: '#EF7057' }}>
+      <Toolbar>
+        <Typography variant="h6" color="inherit" noWrap>
+          Manala Web UI
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    <main>
+      <Outlet />
+    </main>
+  </ThemeProvider>;
 }
